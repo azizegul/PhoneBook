aed7dc6853661a60572dd4d73186b87f9367929f
Azize Aydoğdu Kara <aydogduazize@gmail.com>
2022-07-30 21:22:14 +0300

    Initial commit.

---
+++ src/Contact/PhoneBook.Contact.Domain/Entities/Contact.cs
@@ -0,0 +1,11 @@
+using PhoneBook.Contact.Domain.Common;
+using PhoneBook.Contact.Domain.Enums;
+
+namespace PhoneBook.Contact.Domain.Entities;
+
+public class Contact : BaseEntity
+{
+    public Guid PersonId { get; set; }
+    public ContactInfoType InfoType { get; set; }
+    public string Info { get; set; }
+}
